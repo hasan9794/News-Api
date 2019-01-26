8db1ac96a3bc09a08a0e95d31b30983140847534
Muhammad Hasan <hasanm9794@gmail.com>
2019-01-26 14:27:48 +0500

    News API

--- sw.js
+++ sw.js
@@ -14,6 +14,23 @@ self.addEventListener('install', event => {
         })
       );
 })
+
+self.addEventListener('activate', function (e) {
+    console.log('[ServiceWorker] Activate');
+    e.waitUntil(
+        caches.keys().then(function (keyList) {
+            return Promise.all(keyList.map(function (key) {
+                if (key !== cacheName) {
+                    console.log('[ServiceWorker] Removing old cache', key);
+                    return caches.delete(key);
+                }
+            }));
+        })
+    );
+    return self.clients.claim();
+});
+
+
 self.addEventListener('fetch', event => {
     const req = event.request;
     const url = new URL(req.url);
